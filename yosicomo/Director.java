17e3d8ab6c13383bd27ed57d2a17848b0b94bbd7
morty <alemv96@hotmail.com>
2016-05-04 20:26:35 -0430

    Update Director.java

--- yosicomo/Director.java
+++ yosicomo/Director.java
@@ -1,14 +1,20 @@
 public class Director extends Role {
 
-  public Director(Role inCharge ,String employement){
-    super(inCharge ,employement);
+  public Director(Role inCharge ,String employement ,float range){
+    super(inCharge ,employement ,range);
   }
 
   public void whoPayTheBill(Bill bill){
 
-    if ((bill.getPrice() >= 15000) && (bill.getPrice() < 50000)){
-        System.out.println("firmo: " + super.getEmployement());
+    if ((bill.getPrice() >= super.getRange()) && (bill.getPrice() < super.getIncharge().getRange())){
+      System.out.println("//////////////////////////////////");
+      System.out.println("firmo: " + super.getEmployement());
+      System.out.println("factura efectuada: ");
+      System.out.println("Precio artefacto " + bill.getPrice());
+      System.out.println("nombre de artefacto: " + bill.getName());
+      System.out.println("//////////////////////////////////");
     }
     else super.getIncharge().whoPayTheBill(bill);
   }
 }
+
