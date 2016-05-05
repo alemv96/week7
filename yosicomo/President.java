9f5b318fdd9ff2853dec4f874603b1485d0600e8
morty <alemv96@hotmail.com>
2016-05-04 20:28:22 -0430

    Update President.java

--- yosicomo/President.java
+++ yosicomo/President.java
@@ -1,12 +1,17 @@
 public class President extends Role{
 
-  public President(Role inCharge, String employement){
-    super(inCharge,employement);
+  public President(Role inCharge, String employement ,float range){
+    super(inCharge ,employement ,range);
   }
 
   public void whoPayTheBill(Bill bill){
-    if (bill.getPrice() >= 150000){
-        System.out.println("firmo: " + super.getEmployement());
+    if (bill.getPrice() >= super.getRange()){
+      System.out.println("//////////////////////////////////");
+      System.out.println("firmo: " + super.getEmployement());
+      System.out.println("factura efectuada: ");
+      System.out.println("Precio artefacto " + bill.getPrice());
+      System.out.println("nombre de artefacto: " + bill.getName());
+      System.out.println("//////////////////////////////////");
     }
 
   }
