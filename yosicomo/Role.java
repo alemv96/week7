872dc214886299fe72cf2b0c5314d8d178c0179a
morty <alemv96@hotmail.com>
2016-05-04 20:29:52 -0430

    Update Role.java

--- yosicomo/Role.java
+++ yosicomo/Role.java
@@ -1,36 +1,27 @@
-//el cargo de cada empleado o trabajador;
-public class Role {
-/*atributos (por ahora)*/
-    public Role inCharge;
-    public String employement;
-    /*public final float minimumValue;//valor minimo con el que el empleado tiene acceso a firmar
-    public final float maximumValue;//valor maximo que tiene el empleado para firmar*/
-
-    public Role (Role inCharge ,String employement){
-        this.inCharge = inCharge;
-        this.employement = employement;
-        /*this.minimumValue = minimumValue;
-        this.maximumValue = maximumValue;*/
-    }
-
-    public Role getIncharge(){
-      return this.inCharge;
-    }
-
-    public String getEmployement(){
-      return this.employement;
-    }
-
-  /*  public int getMinimum(){
-      return this.minimumValue;
-    }
-
-    public int getMaximum(){
-      return this,maximumValue;
-    }*/
-
-    public void whoPayTheBill(Bill bill){
-
-    }
-
+public abstract class Role {
+  /*atributos (por ahora)*/
+  public Role inCharge;
+  public String employement;
+  public float range;//limite de empleado para pagar algun articulo.
+  
+  public Role (Role inCharge ,String employement ,float range){
+    this.inCharge = inCharge;
+    this.employement = employement;
+    this.range = range;
+  }
+  
+  public Role getIncharge(){
+    return this.inCharge;
+  }
+  
+  public String getEmployement(){
+    return this.employement;
+  }
+  
+  public float getRange(){
+    return this.range;
+  }
+  
+  public abstract void whoPayTheBill(Bill bill);
+  
 }
